86a398669ed52179bb7b172811baf358406eaeb0
paniklas <paniklas@hotmail.com>
2026-04-16 17:37:10 +0300

    initial commit - setup dependencies

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import LoadingProject from "@/components/Loading/loadding-project";
+
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl">
+      <LoadingProject />
+    </div>
+  )
+}
+
+export default Home
